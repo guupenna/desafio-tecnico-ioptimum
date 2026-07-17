1727786c09dba73d0af2b5a4e43e36cf0f7bc7c4
Gustavo <gupenna1@gmail.com>
2026-07-17 00:00:00 -0300

    feat: implementa arquitetura do projeto e separação de classes, e armazena dados em classes

---
+++ src/classes.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+
+@dataclass
+class Tarefa:
+    nome: str
+    habilidade: str
+    duracao: int
+    qtd: int
+    horas: int  # adiciona atributo de total de horas da habilidade na tarefa
+
+@dataclass
+class OS:
+    id: str
+    tarefas: list[Tarefa]
+    condicao: str
+    prioridade: str
+    pred: str
---
+++ src/loader.py
@@ -0,0 +1,62 @@
+import pandas as pd
+from classes import Tarefa, OS
+
+# Lê cada aba do arquivo Excel como um dataframe
+def load_data(excel_path):
+    os_df = pd.read_excel(excel_path, sheet_name='OS').fillna(None) # tratar colunas vazias
+    tarefas_df = pd.read_excel(excel_path, sheet_name='Tarefas')
+    recursos_df = pd.read_excel(excel_path, sheet_name='Recursos')
+    paradas_df = pd.read_excel(excel_path, sheet_name='Paradas')
+
+    ## Dicionário de tarefas criado para otimizar a busca de tarefas pertencentes à OS específica ao preencher o atributo 'tarefas' da classe OS
+    tarefas_os_dict = {}
+
+    for os_id, grupo in tarefas_df.groupby('OS'):
+        tarefa_obj = [
+            Tarefa(
+                nome=tarefa['Tarefa'],
+                habilidade=tarefa['Habilidade'],
+                duracao=tarefa['Duração'],
+                qtd=tarefa['Quantidade'],
+                horas=tarefa['Duração']*tarefa['Quantidade']
+            )
+
+            for _, tarefa in grupo.iterrows()
+        ]
+
+        tarefas_os_dict[os_id] = tarefa_obj
+
+    ## Cria uma lista de objetos OS com o atributo 'tarefas' sendo as tarefas daquela OS estruturadas em lista
+    os_list = []
+
+    for _, linha in os_df.iterrows():
+        os_obj = OS(
+            id=linha['OS'],
+            tarefas=tarefas_os_dict[linha['OS']],
+            condicao=linha['Condição'],
+            prioridade=linha['Prioridade'],
+            pred=linha['Predecessora']
+        )
+
+        os_list.append(os_obj)
+
+    # Usa set para organizar paradas por questão de simplicidade de uso
+    paradas_set = set(paradas_df['Dia'])
+
+    # Organiza recursos (horas de habilidade por dia da semana) em um dicionário de dicionários
+    # com a chave sendo o dia da semana, para facilitar acesso de horas da habilidade no dia 
+    recursos_dict = {}
+
+    for _, linha in recursos_df.iterrows():
+        dia_num = int(linha['Dia'].split('_')[1])
+
+        habilidade = linha['Habilidade']
+        horas = linha['HH_Disponivel']
+
+        if dia_num not in recursos_dict:
+            recursos_dict[dia_num] = {}
+
+        recursos_dict[dia_num][habilidade] = horas
+
+
+    return os_list, recursos_dict, paradas_set
