eb583afbb382463c7111801d37696ae96e605631
Gustavo <gupenna1@gmail.com>
2026-07-18 14:48:54 -0300

    feat: implementa heuristica construtiva gulosa para soluçao base

--- src/scheduler.py
+++ src/scheduler.py
@@ -1,5 +1,7 @@
 from classes import OS
+from copy import deepcopy
 
+PRIORIDADE_RANKING = {'Z': 0, 'A': 1, 'B': 2, 'C': 3}
 
 def layout_os(
         os: OS,
@@ -10,12 +12,12 @@ def layout_os(
 
     Recebe OS e dia de inicio e monta um layout, que é basicamente a disposição das tarefas da OS nos dias a partir do dia de inicio
 
-    Parameters:
-    os (OS): objeto contendo os dados de uma OS
+    Args:
+    os (OS): dados de uma OS
     dia_inicio (int): dia de inicio da OS passada como parametro
 
-    Returns:
-    dict[tuple[int, str], int] | None: horas consumidas de cada habilidade da OS em cada dia
+    Return:
+    dict[tuple[int, str], int] | None: horas consumidas de cada habilidade da OS em cada dia a partir do dia de inicio
     """
     consumo_os = {}
     livre = 8
@@ -47,7 +49,7 @@ def verifica_consumo(
         consumo_os: dict[tuple[int, str], int], horas_disponiveis: dict[int, dict[str, int]]
 ) -> bool:
     """
-    Verifica se horas usadas pelas tarefas da OS cabem nas horas disponíveis 
+    Verifica se horas usadas pelas tarefas da OS cabem nas horas disponíveis
     """
     for (dia, habilidade), horas_usadas in consumo_os.items():
         if horas_usadas > horas_disponiveis[dia][habilidade]:
@@ -59,10 +61,10 @@ def verifica_consumo(
 def verifica_parada(
         os: OS,
         consumo_os: dict[tuple[int, str], int],
-        paradas: set{int}
+        paradas: set[int]
 ) -> bool:
     """
-    Verifica se os dias ocupados por uma OS de parada são apenas os dias de parada
+    Verifica se os dias ocupados por uma OS de parada são apenas os dias de parada.
     """
     if os.condicao != 'Parada':
         return True
@@ -72,3 +74,87 @@ def verifica_parada(
     return dias_ocupados.issubset(paradas)
 
 
+
+def monta_schedule(
+        os_list: list[OS],
+        recursos_dict: dict[int, dict[str, int]], paradas_set: set[int]
+) -> tuple[
+        dict[str, int],
+        dict[str, int],
+        dict[int, dict[str, int]]
+    ]:
+    """
+    Obtem solução inicial de disposição de dias de início para cada OS
+
+    Args:
+    os_list (list[OS]): dados de todas as OS
+    recursos_dict (dict[int, dict[str, int]]): horas disponiveis de cada habilidade em cada dia
+    paradas_set (set[int]): dias de parada
+
+    Returns:
+    tuple[
+        dict[str, int],
+        dict[str, int],
+        dict[int, dict[str, int]]
+    ]: dias de início para cada OS, dia de fim para cada OS e capacidade restante de cada hora de habilidade dada a alocação de dias
+    """
+
+    # copia dicionario de recursos usando deepcopy para garantir que o dicionario interno tambem seja copiado
+    capacidade = deepcopy(recursos_dict)
+
+    # cria lista de OS ordenada pelos critérios de, respectivamente, prioridade e horas totais, os dois em ordem crescente
+    os_ordenada = sorted(os_list, key=lambda os: (PRIORIDADE_RANKING[os.prioridade], os.horas_total))
+
+    solucao = {}
+
+    # define dicionario usado para lidar com a restrição de predecessora, verificando onde a predecessora acabou por meio deste
+    dia_fim = {}
+
+    # define flag usada para marcar se houve alocação de OS na iteração
+    # é feito isso para tratar casos que é alocada uma OS que tem predecessora, mas a predecessora está mais à frente na lista ordenada 
+    houve_progresso = True
+
+    while houve_progresso:
+        houve_progresso = False
+
+        for os in os_ordenada:
+            if os.id in solucao:
+                continue
+
+            if os.pred is None:
+                dia_min = 1
+            else:
+                if os.pred not in solucao:
+                    continue
+
+                # define o dia minimo de inicio da OS como o dia seguinte ao fim da predecessora
+                dia_min = dia_fim[os.pred] + 1
+
+            # itera por cada dia possível (dia mínimo de início até o último dia da semana, dia 5)
+            for dia_inicio in range(dia_min, 6):
+                consumo_os = layout_os(os, dia_inicio)
+
+                # verifica se o consumo da OS não cabe começando do dia de início
+                if consumo_os is None:
+                    continue
+
+                if not verifica_parada(os, consumo_os, paradas_set):
+                    continue
+
+                if not verifica_consumo(consumo_os, capacidade):
+                    continue
+
+                # subtrai a as horas usadas por cada dia e cada habilidade da OS pela da capacidade atual
+                for (d, habilidade), horas in consumo_os.items():
+                    capacidade[d][habilidade] -= horas
+
+                # atribui dia de fim como o maior valor de dia para os dias de execução de tarefas da OS
+                dia_fim[os.id] = max(d for d,_ in consumo_os)
+
+                solucao[os.id] = dia_inicio
+
+                houve_progresso = True
+
+                break
+
+    return solucao, dia_fim, capacidade
